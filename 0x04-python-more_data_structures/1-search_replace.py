66baab33fc67cd17b822f6a633d46e7776596a1b
Vivi-anne <viviannekwamboka98@gmail.com>
2023-03-15 16:51:34 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!usr/bin/python3
 def search_replace(my_list, search, replace):
+    """
+    A function that replaces all occurence
+    of an element by another in a new list
+    """
     new_list = []
     for element in my_list:
         if element == search:
