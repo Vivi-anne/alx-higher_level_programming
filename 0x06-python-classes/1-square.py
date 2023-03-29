7285ae05f9ad41d54af448b5c9aa47bc18442f31
Vivi-anne <viviannekwamboka98@gmail.com>
2023-03-28 17:25:38 -0700

    changes to square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,5 +4,5 @@
 class Square:
     """ defines a function called __init__ """
     def __init__(self, size):
-        """ initializes __size of self with size """
+        """ initializes __size of self with size"""
         self.__size = size
