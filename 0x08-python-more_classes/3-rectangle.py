a26493463f4ba5ee178002aab8fea2ab3c5c1713
Vivi-anne <viviannekwamboka98@gmail.com>
2023-04-03 15:52:29 -0700

    string representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""Defines an empty class Rectangle that defines a rectangle """
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle with a given width and height.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets/sets the width of this Rectangle.
+        Returns:
+            int: The width of this Rectangle.
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """Gets/sets the height of this Rectangle.
+        Returns:
+            int: The height of this Rectangle.
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    def area(self):
+        """Returns:
+            int: The perimeter of this Rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns:
+            int: The perimeter of this Rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Returns a printable representation of a Rectangle.
+
+        Prints the rectangle with the character #"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
