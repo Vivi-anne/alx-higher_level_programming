98f094f3bf2ad8a3a059c319d234d77136187712
Vivi-anne <viviannekwamboka98@gmail.com>
2023-04-03 15:20:48 -0700

    defining rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,8 @@ class Rectangle:
     """Represents a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """Initialize a new Rectangle with a given width and height.
+
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -14,34 +15,36 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """get/set the height of the rectangle"""
-            return self._height
+    @property
+    def width(self):
+        """Gets/sets the width of this Rectangle.
+        Returns:
+            int: The width of this Rectangle.
+        """
+        return self.__width
 
-        @height.setter
-        def height(self, value):
+    @property
+    def height(self):
+        """Gets/sets the height of this Rectangle.
+        Returns:
+            int: The height of this Rectangle.
         """
-            Checking for TypeError and ValueError and sets up the private var
-        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
 
-        @property
-        def width(self):
-            """get/set the width of the rectangle"""
-            return self._width
-
-        @width.setter
-        def width(self, value):
-        """
-            Checking for TypeError and ValueError and sets up the private var
-        """
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
