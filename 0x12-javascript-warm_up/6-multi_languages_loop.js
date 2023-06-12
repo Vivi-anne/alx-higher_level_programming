a46002ff53c94bef187b9a4d1aa0e859e8a6800b
Vivi-anne <viviannekwamboka98@gmail.com>
2023-06-12 15:28:45 -0700

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const line = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let index = 0; index < line.length; index++){
-	console.log(line[index]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
