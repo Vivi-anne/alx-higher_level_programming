87136dd3c76edb692d70c71bd6905b34d8e96eca
Vivi-anne <viviannekwamboka98@gmail.com>
2023-06-18 10:12:20 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
-                like 'N%' ORDER BY states.id""")
+                REGEXP '^N' ORDER BY states.id""")
     rows = cur.fetchall()
     for row in rows:
         if ("N" in row[1]):
