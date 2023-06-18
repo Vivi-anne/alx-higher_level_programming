13318978b4c92b29324e11f0fad1b2703d2f78b5
Vivi-anne <viviannekwamboka98@gmail.com>
2023-06-18 10:24:01 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N from the database hbtn_0e_0_usa"""
+""" 
+lists all states with a name starting with N from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
@@ -8,7 +10,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
-                REGEXP '^N' ORDER BY states.id""")
+                LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
     for row in rows:
         if ("N" in row[1]):
