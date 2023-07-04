90fa358778653ee4a8e4b7d8595743399d7ff73b
Vivi-anne <viviannekwamboka98@gmail.com>
2023-07-04 15:40:18 -0700

    python network 1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that send request to URL & display response body
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
