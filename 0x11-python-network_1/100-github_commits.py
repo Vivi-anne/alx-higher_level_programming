875beb04066e83896256a8dfcba8f853fd6ec489
Vivi-anne <viviannekwamboka98@gmail.com>
2023-07-04 15:49:12 -0700

    python network 1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,27 @@
-#!/usr/bin/piython3
-"""script that lists the 10 most recent commits on a given GitHub repository.
+#!/usr/bin/python3
 """
-import sys
-import requests
+This script list last 10 commits of a given GitHub repo
+"""
 
 
-if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+def main():
+    """
+    List last 10 commits of given GitHub repository using
+    GitHub API
+    """
+    import requests
+    from sys import argv
 
-    res = requests.get(url)
-    commits = res.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    repo, owner = argv[1], argv[2]
+    URL = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    res = requests.get(URL)
+    max = 10
+    for cmt in res.json():
+        print("{}: {}".format(cmt['sha'], cmt['commit']['author']['name']))
+        max -= 1
+        if max <= 0:
+            break
+
+
+if __name__ == '__main__':
+    main()
