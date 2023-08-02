2ab3d3c2752182b5a34be5f4ec291bdb5a337f5a
Vivi-anne <viviannekwamboka98@gmail.com>
2023-08-02 13:03:09 -0700

    JavaScript - Web jQuery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+$('document').ready(function () {
+    $('DIV#toggle_header').click(function () {
+        if ($('header').hasClass('green')) {
+            $('header').removeClass('green').addClass('red');
+        } else {
+            $('header').removeClass('red').addClass('green');
+        }
+    });    
+});
